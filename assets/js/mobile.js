9415417f87baeb964e975583b9ff7ea5b1bf8aba
Fabián Huertas <miloh9318@hotmail.com>
2025-02-15 17:56:50 -0500

    javascript para redimensionar y abrir y cerrar el menu

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", (e) =>{
 
-    // Seleccionar mis dos elememtos principales
+    // Seleccionar mis elememtos principales
     let mobileBtn = document.querySelector(".navbar__mobile-btn");
-    let mobileMenu= document.querySelector(".menu-mobile");
+    let mobileMenu = document.querySelector(".menu-mobile");
+    let btnClose = document.querySelector(".menu-mobile__close");
 
-    mobileBtn.addEventListener("click", () =>{
+    // Funcion mostrar y ocultar menu
+    const showHiddenMenu = () => {
         let show = document.querySelector(".menu-mobile--show");
 
         if(show){
@@ -12,5 +14,20 @@ document.addEventListener("DOMContentLoaded", (e) =>{
         }else{
             mobileMenu.classList.add("menu-mobile--show")  
         }
+    };
+    
+    // Al dar click al boton del menu mostrar el menu de navegacion responsive
+    mobileBtn.addEventListener("click", showHiddenMenu);
+
+    // Al redimensionar la pantalla ocultar el menu si es necesario
+    window.addEventListener("resize", () => {
+        let windowWidth = parseInt(document.body.clientWidth);
+
+        if(windowWidth >= 1000){
+            mobileMenu.classList.remove("menu-mobile--show");
+        }
     });
-})
+
+    // Poder cerrar el menu con el boton X
+    btnClose.addEventListener("click", showHiddenMenu);     
+});

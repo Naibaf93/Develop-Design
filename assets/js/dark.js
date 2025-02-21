ec8750d1cb7c40483c6a788ff762ec2220b3a03f
Fabián Huertas <miloh9318@hotmail.com>
2025-02-21 10:22:52 -0500

    cambiando el logo usando js

--- assets/js/dark.js
+++ assets/js/dark.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     let switcher = document.querySelector(".switcher__btn");
     let light = document.querySelector(".switcher__icon-light");
     let dark = document.querySelector(".switcher__icon-dark");
+    let logoLight = document.querySelector(".navbar__logo-light");
+    let logoDark = document.querySelector(".navbar__logo-dark");
 
     switcher.addEventListener("click", () => {
         let head = document.head;
@@ -15,12 +17,18 @@ document.addEventListener("DOMContentLoaded", () => {
         let themeDark = document.querySelector("#theme-dark");
         if(themeDark){
             head.removeChild(themeDark);
-            light.style.display = "block"
-            dark.style.display = "none"
+            light.style.display = "block";
+            dark.style.display = "none";
+            logoLight.style.display = "block";
+            logoDark.style.display = "none";
+
         }else{
             head.appendChild(link);
-            light.style.display = "none"
-            dark.style.display = "block"
+            light.style.display = "none";
+            dark.style.display = "block";
+            logoLight.style.display = "none";
+            logoDark.style.display = "block";
+
         }
     });
 })
